03f2288fbdfaeb28f5245a7555f0113bdd951f19
fmixtur <fmixtur@student.42lausanne.ch>
2026-02-11 13:48:58 +0100

    Remove unused set.pmn file and update array.h and main.cpp includes

--- ex02/array.h
+++ ex02/array.h
@@ -5,23 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmixtur <fmixtur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/10 15:00:00 by fmixtur           #+#    #+#             */
-/*   Updated: 2026/02/10 15:38:47 by fmixtur          ###   ########.ch       */
+/*   Created: 2026/02/11 13:42:51 by fmixtur           #+#    #+#             */
+/*   Updated: 2026/02/11 13:42:51 by fmixtur          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include <cmath>
+#include <cstddef>
+#include <stdexcept>
 
 template <typename T>
 class Array
 {
     private:
+
     T* _array;
     unsigned int _size;
+
+
     public:
+
     Array() : _array(NULL), _size(0) {}
+
     Array(unsigned int n) : _array(new T[n]()), _size(n) {}
+
     Array(const Array& other) : _array(new T[other._size]()), _size(other._size) {
         for (unsigned int i = 0; i < _size; i++)
             _array[i] = other._array[i];
@@ -40,7 +47,7 @@ class Array
     }
     T& operator[](unsigned int index) {
         if (index >= _size)
-            throw std::out_of_range("Index out of range");
+            throw std::out_of_range("  out of range");
         return _array[index];
     }
     const T& operator[](unsigned int index) const {
