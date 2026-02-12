e1728d4241307cf4bf2b498c134045176790cdd1
fmixtur <fmixtur@student.42lausanne.ch>
2026-02-12 11:57:52 +0100

    Update Makefile and main.cpp for Array class implementation; set program name to 'array'

--- ex02/array.h
+++ ex02/array.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmixtur <fmixtur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/11 13:42:51 by fmixtur           #+#    #+#             */
-/*   Updated: 2026/02/11 13:42:51 by fmixtur          ###   ########.ch       */
+/*   Created: 2026/02/12 11:46:53 by fmixtur           #+#    #+#             */
+/*   Updated: 2026/02/12 11:49:10 by fmixtur          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,54 @@
 template <typename T>
 class Array
 {
-    private:
+	private:
 
-    T* _array;
-    unsigned int _size;
+	T* _array;
+	unsigned int _size;
 
 
-    public:
+	public:
 
-    Array() : _array(NULL), _size(0) {}
+	Array() : _array(NULL), _size(0) {}
 
-    Array(unsigned int n) : _array(new T[n]()), _size(n) {}
+	Array(unsigned int n) : _array(new T[n]()), _size(n) {}
 
-    Array(const Array& other) : _array(new T[other._size]()), _size(other._size) {
-        for (unsigned int i = 0; i < _size; i++)
-            _array[i] = other._array[i];
-    }
-    ~Array() { delete[] _array; 
-    }
-    Array& operator=(const Array& other) {
-        if (this != &other) {
-            delete[] _array;
-            _size = other._size;
-            _array = new T[_size]();
-            for (unsigned int i = 0; i < _size; i++)
-                _array[i] = other._array[i];
-        }
-        return *this;
-    }
-    T& operator[](unsigned int index) {
-        if (index >= _size)
-            throw std::out_of_range("  out of range");
-        return _array[index];
-    }
-    const T& operator[](unsigned int index) const {
-        if (index >= _size)
-            throw std::out_of_range("Index out of range");
-        return _array[index];
-    }
-    unsigned int size() const { return _size; }
+	Array(const Array& other) : _array(new T[other._size]()), _size(other._size)
+	{
+		for (unsigned int i = 0; i < _size; i++)
+			_array[i] = other._array[i];
+	}
+	
+	~Array() 
+	{
+		delete[] _array; 
+	}
+
+	Array& operator=(const Array& other)
+	{
+		if (this != &other) {
+			delete[] _array;
+			_size = other._size;
+			_array = new T[_size]();
+			for (unsigned int i = 0; i < _size; i++)
+				_array[i] = other._array[i];
+		}
+		return *this;
+	}
+	
+	T& operator[](unsigned int index)
+	{
+		if (index >= _size)
+			throw std::out_of_range("  out of range");
+		return _array[index];
+	}
+
+	const T& operator[](unsigned int index) const
+	{
+		if (index >= _size)
+			throw std::out_of_range("Index out of range");
+		return _array[index];
+	}
+
+	unsigned int size() const { return _size; }
 };
